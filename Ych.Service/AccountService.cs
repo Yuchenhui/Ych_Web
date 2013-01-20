16686e0e44918edde95a4314aa62f54b0c376d35
Marshall <yuchenhui@vip.sina.com>
2013-01-21 03:10:42 +0800

    Ych.im

--- Ych.Service/AccountService.cs
+++ Ych.Service/AccountService.cs
@@ -10,8 +10,14 @@ namespace Ych.Service
 {
     public class AccountService : IAccount
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
         public bool CreateAccount(Account account)
         {
+            int x = 0;
             var collection = MongoDb.Instance.GetCollection<Account>();
             var result = collection.Insert(account);
             return result.Ok;
